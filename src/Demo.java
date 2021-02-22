2411af963cfeb04039148300f63e53b6bc9003be
manoj <manoj.sarwate@ssiedu.in>
2021-02-22 12:54:37 +0530

    22-02-21

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+
+		
+	public static void main(String args[]) {
+		
+		System.out.println("hello from main ");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		//System.out.println(args[3]);
+		
+	}
+	
+
+}
